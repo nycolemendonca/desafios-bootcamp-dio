478838e405953255abf09b213c3193f24d38fe83
Nycole <62551765+nycolemendonca@users.noreply.github.com>
2022-06-22 11:46:16 -0300

    Add files via upload

---
+++ sintaxe-e-operadores-js/operadores.js
@@ -0,0 +1,50 @@
+/*
+    - const: constante; variável que não pode ser redesignada.
+    - let: variável local; declarada no início da função, de preferência.
+    - var: variável global.
+
+*/
+
+function comparaNumeros(num1, num2) {
+    const primeiraFrase = criaPrimeiraFrase(num1, num2);
+    const segundaFrase = criaSegundaFrase(num1, num2);
+
+    return `${primeiraFrase} ${segundaFrase}`;
+}
+
+function criaPrimeiraFrase(num1, num2) {
+    let primeiraFrase = `Os números ${num1} e ${num2}`;
+    let simNao = 'não';
+
+    if (num1 === num2) {
+        simNao = '';
+    }
+
+    return `${primeiraFrase} ${simNao} são iguais.`;
+}
+
+function criaSegundaFrase(num1, num2) {
+    const soma = num1 + num2;
+    let comparaDez = 'menor do que';
+    let comparaVinte = 'menor do que';
+
+    if (soma > 10) {
+        comparaDez = 'maior do que';
+    }
+
+    if (soma == 10) {
+        comparaDez = 'igual a';
+    }
+
+    if (soma == 20) {
+        comparaVinte = 'igual a';
+    }
+
+    if (soma > 20) {
+        comparaVinte = 'maior do que';
+    }
+
+    return `Sua soma é ${soma}, que é ${comparaDez} 10 e ${comparaVinte} 20.`;
+}
+
+console.log(comparaNumeros(10, 10));
