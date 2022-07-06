3d6a2ef805f62232f84264891fe7f22b52120414
Nycole <62551765+nycolemendonca@users.noreply.github.com>
2022-07-06 11:44:21 -0300

    Add files via upload

---
+++ utilizando-modulos-js/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby  } from "./funcoes.mjs";
+
+console.log(mostraIdade('Nycole', 21));
+console.log(mostraCidade('Nycole', 'Fortaleza'));
+console.log(mostraHobby('Nycole', 'ler livros'));
